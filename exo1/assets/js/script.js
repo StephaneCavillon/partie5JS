619c643742828066a2c81a918df990d47ad90fec
StephaneC <stephane.cavillon@itech.fr>
2020-11-04 16:52:30 +0100

    Fin exercice1

---
+++ exo1/assets/js/script.js
@@ -0,0 +1,16 @@
+//écouter ce qu'il se passe quand on click sur vérification
+
+document.getElementById('check').addEventListener('click', function checking(){
+    
+    // déclaration des variables avec le contenu des inputs
+    let password = document.getElementById('password').value;
+    let passwordConfirm = document.getElementById('passwordConfirm').value;
+
+    if(password === passwordConfirm){
+        document.getElementById('password').style.borderColor = 'green';
+        document.getElementById('passwordConfirm').style.borderColor = 'green';
+    } else { 
+        document.getElementById('password').style.borderColor = 'red';
+        document.getElementById('passwordConfirm').style.borderColor = 'red';
+    }
+});
