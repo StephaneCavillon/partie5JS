ea0d7faadfa23011114751bd15198838ff0f6ed7
StephaneC <stephane.cavillon@itech.fr>
2020-11-06 00:58:55 +0100

    fin de l'exo 3 nettoyé

--- exo3/assets/js/script.js
+++ exo3/assets/js/script.js
@@ -7,15 +7,9 @@ const regexAge = /\d{1,3}/;
 
 //Pour le Nom
 const validName = function (inputName){
-    // On va chercher la valeur du champ Email
-    inputName = document.getElementById('name').value;
-
-    //on verifie que la valeur est conforme à la regex
-    let testName = regexName.test(inputName);
-    console.log('name ' + inputName);
-    console.log('retour regex ' + testName);
-
-    if(testName){
+    
+    // On teste la regex
+    if(regexName.test(inputName.value)){
         document.getElementById('valideName').innerHTML = 'Nom Valide';
         document.getElementById('valideName').style.color = 'green';
     } else {
@@ -26,15 +20,10 @@ const validName = function (inputName){
 
 //Pour le mail
 const validEMail = function (inputEmail){
-    // On va chercher la valeur du champ Email
-    inputEmail = document.getElementById('mail').value;
 
-    //on verifie que la valeur est conforme à la regex
-    let testMail = regexMail.test(inputEmail);
-    console.log(inputEmail);
-    console.log('retour ' + testMail);
+    // On teste la regex
 
-    if(testMail){
+    if(regexMail.test(inputEmail.value)){
         document.getElementById('valideMail').innerHTML = 'Email Valide';
         document.getElementById('valideMail').style.color = 'green';
     } else {
@@ -46,15 +35,9 @@ const validEMail = function (inputEmail){
 
 // Pour l'age
 const validAge = function (inputAge){
-    // On va chercher la valeur du champ Email
-    inputAge = document.getElementById('age').value;
 
-    //on verifie que la valeur est conforme à la regex
-    let testAge = regexAge.test(inputAge);
-    console.log(inputAge);
-    console.log( 'retour ' + testAge);
-
-    if(testAge){
+    // On teste la regex
+    if(regexAge.test(inputAge.value)){
         document.getElementById('valideAge').innerHTML = 'Age Valide';
         document.getElementById('valideAge').style.color = 'green';
     } else {
@@ -64,7 +47,7 @@ const validAge = function (inputAge){
 };
 
 // Création des listeners
-
+// Le fait de mettre this dans la fonction fait appel à l'element sur lequel on a posé le listener
 //Pour le nom
 document.getElementById('name').addEventListener('change',function() {
     validName(this);
