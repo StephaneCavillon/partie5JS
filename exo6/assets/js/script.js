d402cc7d0528c2127a50bcd258f9680a9c34112f
StephaneC <stephane.cavillon@itech.fr>
2020-11-09 11:14:06 +0100

    fin exo6

--- exo6/assets/js/script.js
+++ exo6/assets/js/script.js
@@ -1,10 +1,10 @@
-// Déclaration des variables
-// avec querySelector on selectionne tout les noeuds qui contiennent des inputs
-let formInput = document.querySelectorAll('input');
+// // Déclaration des variables
+// // avec querySelector on selectionne tout les noeuds qui contiennent des inputs
+// let formInput = document.querySelectorAll('input');
 
 // Déclaration des listeners
 // forEach permet d'appliquer le listener sur tout les items du querySelector
-formInput.forEach(item=> { 
+document.querySelectorAll('input').forEach(item=> { 
     item.addEventListener('focus', function(){
         this.classList.add('onFocus');
     });
@@ -12,7 +12,7 @@ formInput.forEach(item=> {
 
 
 
-formInput.forEach(item=>{
+document.querySelectorAll('input').forEach(item=>{
     item.addEventListener('blur',function(){
     this.classList.remove('onFocus');
     });
