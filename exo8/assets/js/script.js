f044ecb27857a74c1c608deda3fb5feb09997f9d
StephaneC <stephane.cavillon@itech.fr>
2020-11-09 17:29:22 +0100

    fin exo8

---
+++ exo8/assets/js/script.js
@@ -0,0 +1,25 @@
+// Déclaration des variables
+let i=1;
+
+// Création de la fonction de clonage
+function duplic(){
+    // Je vais chercher la variable i et je l'incrémente de 1 à chaque appel de la fonction
+    i++;
+    // Je créé un clone de mon noeud avec tout ce qu'il contient (true)
+    const clone = document.getElementById('champ').cloneNode(true);
+    // Je place le clone dans le premier item du fieldset
+    document.getElementsByTagName('fieldset').item(0).appendChild (clone);
+
+    // Je change l'id des clones créé
+    //d'abord j'incrémente l'id déjà existante
+    clone.id = clone.id + i;
+    // je remplace le placeholder des input
+    clone.children.item(0).setAttribute('placeholder','');
+    clone.children.item(2).setAttribute('placeholder','');
+
+};
+
+// //création du Listener
+document.getElementById('add').addEventListener('click', function() {
+    duplic();
+});
