36f57e01d12cec5b551012976cd895fc67905ff0
StephaneC <stephane.cavillon@itech.fr>
2020-11-10 09:36:28 +0100

    fin des exos

--- exo8/assets/js/script.js
+++ exo8/assets/js/script.js
@@ -5,8 +5,10 @@ let i=1;
 function duplic(){
     // Je vais chercher la variable i et je l'incrémente de 1 à chaque appel de la fonction
     i++;
+
     // Je créé un clone de mon noeud avec tout ce qu'il contient (true)
     const clone = document.getElementById('champ').cloneNode(true);
+
     // Je place le clone dans le premier item du fieldset
     document.getElementsByTagName('fieldset').item(0).appendChild (clone);
 
@@ -15,7 +17,9 @@ function duplic(){
     clone.id = clone.id + i;
     // je remplace le placeholder des input
     clone.children.item(0).setAttribute('placeholder','');
+    clone.children.item(0).setAttribute('value','');
     clone.children.item(2).setAttribute('placeholder','');
+    clone.children.item(2).setAttribute('value','');
 
 };
 
